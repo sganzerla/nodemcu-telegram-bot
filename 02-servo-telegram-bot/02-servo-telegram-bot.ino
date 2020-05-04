62464b691e499de68f0f7d0ca1e3f3d3bca5f1d2
alisson sganzerla <alisson_sganzerla@yahoo.com.br>
2020-05-04 00:54:12 -0300

    awesome commit, funcionou muito bem

--- 02-servo-telegram-bot/02-servo-telegram-bot.ino
+++ 02-servo-telegram-bot/02-servo-telegram-bot.ino
@@ -1,4 +1,25 @@
+// biblioteca para conexao WiFi ESP8286
+#include <ESP8266WiFi.h>
+// biblioteca para utilizar client SSL
+#include <WiFiClientSecure.h>
+// biblioteca para manipular BOT Telegram
+// lamentavelmente a versao exige que o ArduinoJson
+// esteja na versao 5.13.5
+#include <TelegramBot.h>
+
 #include <Servo.h>
+
+// nome da Rede WIFI
+const char *ssid = "";
+// senha da Rede WIFI
+const char *password = "";
+
+// token BOT Telegram
+const char BotToken[] = "";
+
+WiFiClientSecure net_ssl;
+
+TelegramBot bot(BotToken, net_ssl);
 
 // instância do servo
 Servo myservo;
@@ -6,8 +27,8 @@ Servo myservo;
 #define SERVO_PIN D3
 
 // limites dos angulos do servo
-// indicam a abertura da tampa em ordem crescente
-const uint8_t ANGLE_SERVO[] = {0, 20, 40, 60, 80};
+// indicam a abertura da tampa em ordem decrescente
+const uint8_t ANGLE_SERVO[] = {80, 60, 40, 20, 0};
 
 // indica se a tampa está em movimento
 bool isMove = false;
@@ -21,10 +42,79 @@ void setup()
 
     // define como fechada a tampa da caixa
     myservo.write(ANGLE_SERVO[0]);
+
+    // inicia conexao WIFI
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println(WiFi.localIP());
+
+    bot.begin();
+    // desabilita conexao segura com SSL
+    net_ssl.setInsecure();
+
+    Serial.print("Angulo Servo: ");
+    Serial.println(angleServo());
 }
 
 void loop()
 {
+
+    delay(200);
+    // checa se há conexão
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        // verifica mensagens no chat
+        message m = bot.getUpdates();
+
+        // verifica se há conteúdo na mensagem
+        if (m.chat_id != 0)
+        {
+            // se a mensagem for OPEN
+            if (m.text.equals("OPEN"))
+            {
+                if (isMove)
+                {
+                    bot.sendMessage(m.chat_id, "Lamento, a tampa já estava em movimento, tente novamente em instantes.");
+                    return;
+                }
+                if (angleServo() == ANGLE_SERVO[4])
+                {
+                    bot.sendMessage(m.chat_id, "Tampa já estava aberta.");
+                    return;
+                }
+                abrir();
+                // ... devolve mensagem no chat
+                bot.sendMessage(m.chat_id, "Tudo certo, já abri a tampa.");
+            }
+            // e se a mensagem for CLOSE ...
+            else if (m.text.equals("CLOSE"))
+            {
+                if (isMove)
+                {
+                    bot.sendMessage(m.chat_id, "Lamento, a tampa já estava em movimento, tente novamente em instantes.");
+                    return;
+                }
+                if (angleServo() == ANGLE_SERVO[0])
+                {
+                    bot.sendMessage(m.chat_id, "Tampa já estava fechada.");
+                    return;
+                }
+                fechar();
+                // ... devolve mensagem no chat
+                bot.sendMessage(m.chat_id, "Pronto, fechei a tampa.");
+            }
+            else
+            {
+                bot.sendMessage(m.chat_id, situacaoTampa());
+            }
+        }
+    }
 }
 
 void abrir()
@@ -56,13 +146,13 @@ char *situacaoTampa()
     if (isMove)
         return "Em movimento.";
 
-    int angle = myservo.read();
+    int angle = angleServo();
 
     if (angle == ANGLE_SERVO[0])
     {
         return "Tampa fechada.";
     }
-    else if (angle == ANGLE_SERVO[80])
+    else if (angle == ANGLE_SERVO[4])
     {
         return "Tampa totalmente aberta.";
     }
@@ -70,4 +160,9 @@ char *situacaoTampa()
     {
         return "Não tenho certeza, mas provavelmente a tampa está parcialmente aberta.";
     }
+}
+
+uint16 angleServo()
+{
+    return myservo.read();
 }
